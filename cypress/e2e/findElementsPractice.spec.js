2125c6770d35eb7ad970c99a167fda6bd0869f5d
pocu46 <andrew.pulyaev@gmail.com>
2022-06-22 22:09:24 +0300

    locators test + how to find elements are added

---
+++ cypress/e2e/findElementsPractice.spec.js
@@ -0,0 +1,26 @@
+describe('Find HTML elements practice', ()=> {
+  it('first find HTML elements test', ()=> {
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.get('[data-cy="signInButton"]')
+
+    cy.contains('Sign in')
+
+    cy.contains('[status="warning"]', 'Sign in')
+
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click()
+
+    cy.contains('nb-card', 'Horizontal form')
+      .find('[type="email"]')
+
+  })
+})
